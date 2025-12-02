5b000461a2bcad48bf1de30afb71be0d6eaf3416
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-12-02 23:09:44 +0100

    Creacion de proyecto monorepo

---
+++ shared/vitest.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  root: __dirname,
+  test: {
+    globals: true,
+    environment: 'node',
+    include: ['src/**/*.spec.ts']
+
+  },
+});
